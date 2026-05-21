5f8e2d6f743234ee4da339b570124a7131d3bc1d
c0pperdragon <c0pperdragon50@gmail.com>
2026-05-21 17:58:28 +0200

    PCB design

--- combine.py
+++ combine.py
@@ -1,5 +1,11 @@
-# small program to find best combination of available resistors to match a given value
-resistors = [15,22,33,47,68,100,150,220,330,470,680,1000,1500,2200,3300,4700,6800,10000,15000,22000,33000,47000,68000,100000,150000,220000,330000,470000]
+# small program to find best combination of resistors in my store to match a given value
+resistors = [
+    75,91,
+    100,130,150,180,270,330,340,390,402,470,499,560,680,
+    1000,1370,1500,2200,2700,3300,4020,4700,5490,6800,8060,
+    10000,16020,
+    100000,220000,
+]
 
 def distance(a,b):
     if a>b:
@@ -12,19 +18,22 @@ def bestcombo(targetvalue):
     for a in resistors:
         combos.append([a])
         for b in resistors:
-#            combos.append(["s",a,b,a+b])
-            combos.append(["p",a,b,1/(1/a+1/b)])
+            combos.append([a,b,1/(1/a+1/b)])
             for c in resistors:
-                combos.append(["p",a,b,c,1/(1/a+1/b+1/c)])
-                
+                combos.append([a,b,c,1/(1/a+1/b+1/c)])
     best = None    
     for x in combos:
         if best==None or distance(x[len(x)-1],targetvalue)<distance(best[len(best)-1],targetvalue):
             best = x
     return best
 
-print(bestcombo(3344))
-print(bestcombo(1703))
-print(bestcombo(8771))
-print(bestcombo(564))
-print(bestcombo(713))
+def check(targetvalue):
+    print ("best for",targetvalue,":",bestcombo(targetvalue))
+    
+check(3344)
+check(1703)
+check(8771)
+check(5490)
+check(1230)
+check(713)
+check(564)
